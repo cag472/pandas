a0d0b84aa8f8a0061bd6b1d6c19cf84a7d8583b1
Christopher A. George <george@physics.ucsb.edu>
2016-03-10 21:46:49 -0800

    analysis

--- ncaa/analyzer.py
+++ ncaa/analyzer.py
@@ -20,6 +20,9 @@ tourney = pd.read_csv('data/TourneyDetailedResults.csv', header = 0)
 #Load in Team Information
 teams = pd.read_csv('data/Teams.csv', header = 0)
 
+#Load in seed information
+seeds = pd.read_csv('data/TourneySeeds.csv', header = 0)
+
 #------------------------    EXPLANATION OF VARIABLES    -------------------------------------
 #Timing information
   #Season - year game was played
@@ -48,142 +51,147 @@ teams = pd.read_csv('data/Teams.csv', header = 0)
 
 #------------------------   FIGURE OUT BEST TEAMS -------------------------------------
 #If a team has been in the top-32 for at least 2 of the last 3 years running, they get class=1
-for j in range(1101, 1465): 
-  number = 0
-  for i in range(year-3, year):
-    nWins = tourney[ (tourney.Season == i) & ((tourney.Wteam == j) | (tourney.Lteam == j))][['Season']].count()
-    if (int(nWins) > 1): number += 1
-  if (number >= 2): 
-    teams.loc[ (teams.Team_Id == j) & (teams.Team_Class == 2), 'Team_Class'] = 1
+for k in [df, tourney]:
+  for j in range(1101, 1465): 
+    number = 0
+    for i in range(year-3, year):
+      nWins = tourney[ (tourney.Season == i) & ((tourney.Wteam == j) | (tourney.Lteam == j))][['Season']].count()
+      if (int(nWins) > 1): number += 1
+    if (number >= 2): 
+      teams.loc[ (teams.Team_Id == j) & (teams.Team_Class == 2), 'Team_Class'] = 1
+    k.loc[ (k.Wteam == j), 'WteamClass'] = teams[ (teams.Team_Id == j)]['Team_Class'].mean()
+    k.loc[ (k.Lteam == j), 'LteamClass'] = teams[ (teams.Team_Id == j)]['Team_Class'].mean()
 
-#------------------------    AFOM FOR EACH TEAM    -------------------------------------
-#Calculate the base FOM for the winner and loser in each game
-df['FOMW'] = (df.Wscore / (df.Wscore + df.Lscore)) 
-numer = (df.Wscore - df.Lscore) - 4
-for i in range(len(numer)):
-  if (numer[i] < 0): numer[i] = 0
-df['FOMW'] += 0.02*numer
-df.loc[ (df.FOMW > 0.95), 'FOMW'] = 0.95
-df['FOML'] = 1-df.FOMW
+#------------------------   DEFINE TEAM 1 AND TEAM 2 FOR EACH GAME -------------------------------------
+for i in range(1101, 1465):
+  df.loc[ (df.Wteam == i) & (df.Wteam > df.Lteam), 'Team2' ] = df.Wteam
+  df.loc[ (df.Wteam == i) & (df.Wteam > df.Lteam), 'Team1' ] = df.Lteam
+  df.loc[ (df.Wteam == i) & (df.Wteam < df.Lteam), 'Team2' ] = df.Lteam
+  df.loc[ (df.Wteam == i) & (df.Wteam < df.Lteam), 'Team1' ] = df.Wteam
+  df.loc[ (df.Wteam == i) & (df.Wteam > df.Lteam), 'Team2score' ] = df.Wscore
+  df.loc[ (df.Wteam == i) & (df.Wteam > df.Lteam), 'Team1score' ] = df.Lscore
+  df.loc[ (df.Wteam == i) & (df.Wteam < df.Lteam), 'Team2score' ] = df.Lscore
+  df.loc[ (df.Wteam == i) & (df.Wteam < df.Lteam), 'Team1score' ] = df.Wscore
+  df.loc[ (df.Wteam == i) & (df.Wteam > df.Lteam), 'Team2class' ] = df.WteamClass
+  df.loc[ (df.Wteam == i) & (df.Wteam > df.Lteam), 'Team1class' ] = df.LteamClass
+  df.loc[ (df.Wteam == i) & (df.Wteam < df.Lteam), 'Team2class' ] = df.LteamClass
+  df.loc[ (df.Wteam == i) & (df.Wteam < df.Lteam), 'Team1class' ] = df.WteamClass
+  df.loc[ (df.Wteam == i) & (df.Wteam < df.Lteam) & (df.Wloc == 'H'), 'Team1isHome' ] = 1
+  df.loc[ (df.Wteam == i) & (df.Wteam > df.Lteam) & (df.Wloc == 'H'), 'Team1isHome' ] = 0
+  df.loc[ (df.Wteam == i) & (df.Wteam < df.Lteam) & (df.Wloc == 'A'), 'Team1isHome' ] = 0
+  df.loc[ (df.Wteam == i) & (df.Wteam > df.Lteam) & (df.Wloc == 'A'), 'Team1isHome' ] = 1
+  df.loc[ (df.Wteam == i) & (df.Wloc == 'N'), 'Team1isHome' ] = 2
+df['ClassDiff'] = df.Team1class - df.Team2class
+df = df.drop( [ 'Wteam', 'Lteam', 'Wscore', 'Lscore', 'WteamClass', 'LteamClass', 'Team1class', 'Team2class' ], axis=1) 
+
+#------------------------    FOM FOR EACH REGULAR GAME    -------------------------------------
+df.loc[ (df.Team1score > df.Team2score), 'FOM1' ] = (df.Team1score / (df.Team1score + df.Team2score)) 
+df.loc[ (df.Team1score > df.Team2score) & (df.Team1score - df.Team2score > 4), 'FOM1' ] += .02*(df.Team1score - df.Team2score - 4)
+df.loc[ (df.Team1score > df.Team2score), 'FOM2' ] = 1-df.FOM1
+df.loc[ (df.Team1score < df.Team2score), 'FOM2' ] = (df.Team2score / (df.Team1score + df.Team2score))
+df.loc[ (df.Team1score < df.Team2score) & (df.Team2score - df.Team1score > 4), 'FOM2' ] += .02*(df.Team2score - df.Team1score - 4)
+df.loc[ (df.Team1score < df.Team2score), 'FOM1' ] = 1-df.FOM2
+df.loc[ (df.FOM1 > 0.95), 'FOM1' ] = 0.95
+df.loc[ (df.FOM2 > 0.95), 'FOM2' ] = 0.95
+df.loc[ (df.FOM1 < 0.05), 'FOM1' ] = 0.05
+df.loc[ (df.FOM2 < 0.05), 'FOM2' ] = 0.05
+
+#------------------------    CALC AFOM FOR EACH TEAM  -------------------------------------
 for i in range(1101,1465):
-  teams.loc[ (teams.Team_Id == i), 'AFOM'] = (df[ (df.Season >= year - 6) & (df.Season != year) & (df.Wteam == i) ]['FOMW'].sum() + df[ (df.Season >= year - 6) & (df.Season != year) & (df.Lteam == i) ]['FOML'].sum())/df[ (df.Season >= year - 6) & (df.Season != year) & ((df.Wteam == i) | (df.Lteam == i))]['FOMW'].count()
+  teams.loc[ (teams.Team_Id == i), 'AFOM1'] =  (df[ (df.Season == year) & (df.Team1 == i)]['FOM1'].sum() + df[ (df.Season == year) & (df.Team2 == i)]['FOM2'].sum())/df[ (df.Season == year) & ((df.Team1 == i) | (df.Team2 == i))]['FOM1'].count()
+  df.loc[ (df.Team1 == i), 'Team1AFOM1']  = teams[ (teams.Team_Id == i)]['AFOM1'].mean()
+  df.loc[ (df.Team2 == i), 'Team2AFOM1']  = teams[ (teams.Team_Id == i)]['AFOM1'].mean()
 
-#---------------------  NOW CALCULATE PRED FOM FOR EACH REGULAR SEASON GAME ------------------------
-df['PFOM'] = -9999
+##---------------------  NOW CALCULATE PRED FOM FOR EACH REGULAR SEASON GAME ------------------------
+#Make base prediction
+df.loc[(abs(df.ClassDiff) < 4), 'PFOM1'] = df.Team1AFOM1/(df.Team1AFOM1+df.Team2AFOM1)
+#Now be more confident in prediction
+df.loc[ (df.PFOM1 > 0.5), 'Error'] = (df.FOM1 - df.PFOM1)/df.PFOM1
+df.loc[ (df.PFOM1 < 0.5), 'Error'] = -(df.FOM1 - df.PFOM1)/df.PFOM1
+corr = df[ (df.ClassDiff == 0) & ((df.Season == year) | (df.Season == year - 1))& (df.Team1AFOM1 > .2) & (df.Team2AFOM1 < .8) ][['Error']].mean()
+df.loc[ (df.PFOM1 > 0.54), 'PFOM1'] *= 1.+float(corr)
+df.loc[ (df.PFOM1 < 0.46), 'PFOM1'] = 1.-(1.-df.PFOM1)*(1.+float(corr))
+#Finally, account for differences in class
+df.loc[ (df.ClassDiff < 0), 'PFOM1'] = df.PFOM1**(1/(2*abs(df.ClassDiff)))
+df.loc[ (df.ClassDiff > 0), 'PFOM1'] = 1.-((1.-df.PFOM1)**(1/(2*abs(df.ClassDiff))))
+
+#--------------------- USE THIS TO MAKE PREDICTIONS FOR 2015 TOURNEY
+for i in range(1101, 1465):
+  tourney.loc[ (tourney.Wteam == i) & (tourney.Wteam > tourney.Lteam), 'Team2' ] = tourney.Wteam
+  tourney.loc[ (tourney.Wteam == i) & (tourney.Wteam > tourney.Lteam), 'Team1' ] = tourney.Lteam
+  tourney.loc[ (tourney.Wteam == i) & (tourney.Wteam < tourney.Lteam), 'Team2' ] = tourney.Lteam
+  tourney.loc[ (tourney.Wteam == i) & (tourney.Wteam < tourney.Lteam), 'Team1' ] = tourney.Wteam
+  tourney.loc[ (tourney.Wteam == i) & (tourney.Wteam > tourney.Lteam), 'Team2score' ] = tourney.Wscore
+  tourney.loc[ (tourney.Wteam == i) & (tourney.Wteam > tourney.Lteam), 'Team1score' ] = tourney.Lscore
+  tourney.loc[ (tourney.Wteam == i) & (tourney.Wteam < tourney.Lteam), 'Team2score' ] = tourney.Lscore
+  tourney.loc[ (tourney.Wteam == i) & (tourney.Wteam < tourney.Lteam), 'Team1score' ] = tourney.Wscore
+  tourney.loc[ (tourney.Wteam == i) & (tourney.Wteam > tourney.Lteam), 'Team2class' ] = tourney.WteamClass
+  tourney.loc[ (tourney.Wteam == i) & (tourney.Wteam > tourney.Lteam), 'Team1class' ] = tourney.LteamClass
+  tourney.loc[ (tourney.Wteam == i) & (tourney.Wteam < tourney.Lteam), 'Team2class' ] = tourney.LteamClass
+  tourney.loc[ (tourney.Wteam == i) & (tourney.Wteam < tourney.Lteam), 'Team1class' ] = tourney.WteamClass
+  tourney.loc[ (tourney.Wteam == i) & (tourney.Wteam < tourney.Lteam) & (tourney.Wloc == 'H'), 'Team1isHome' ] = 1
+  tourney.loc[ (tourney.Wteam == i) & (tourney.Wteam > tourney.Lteam) & (tourney.Wloc == 'H'), 'Team1isHome' ] = 0
+  tourney.loc[ (tourney.Wteam == i) & (tourney.Wteam < tourney.Lteam) & (tourney.Wloc == 'A'), 'Team1isHome' ] = 0
+  tourney.loc[ (tourney.Wteam == i) & (tourney.Wteam > tourney.Lteam) & (tourney.Wloc == 'A'), 'Team1isHome' ] = 1
+  tourney.loc[ (tourney.Wteam == i) & (tourney.Wloc == 'N'), 'Team1isHome' ] = 2
+tourney['ClassDiff'] = tourney.Team1class - tourney.Team2class
+tourney = tourney.drop( [ 'Wteam', 'Lteam', 'Wscore', 'Lscore', 'WteamClass', 'LteamClass', 'Team1class', 'Team2class' ], axis=1) 
+
+#------------------------    FOM FOR EACH REGULAR GAME    -------------------------------------
+tourney.loc[ (tourney.Team1score > tourney.Team2score), 'FOM1' ] = (tourney.Team1score / (tourney.Team1score + tourney.Team2score)) 
+tourney.loc[ (tourney.Team1score > tourney.Team2score) & (tourney.Team1score - tourney.Team2score > 4), 'FOM1' ] += .02*(tourney.Team1score - tourney.Team2score - 4)
+tourney.loc[ (tourney.Team1score > tourney.Team2score), 'FOM2' ] = 1-tourney.FOM1
+tourney.loc[ (tourney.Team1score < tourney.Team2score), 'FOM2' ] = (tourney.Team2score / (tourney.Team1score + tourney.Team2score))
+tourney.loc[ (tourney.Team1score < tourney.Team2score) & (tourney.Team2score - tourney.Team1score > 4), 'FOM2' ] += .02*(tourney.Team2score - tourney.Team1score - 4)
+tourney.loc[ (tourney.Team1score < tourney.Team2score), 'FOM1' ] = 1-tourney.FOM2
+tourney.loc[ (tourney.FOM1 > 0.95), 'FOM1' ] = 0.95
+tourney.loc[ (tourney.FOM2 > 0.95), 'FOM2' ] = 0.95
+tourney.loc[ (tourney.FOM1 < 0.05), 'FOM1' ] = 0.05
+tourney.loc[ (tourney.FOM2 < 0.05), 'FOM2' ] = 0.05
+
+#Load in teams AFOM
 for i in range(1101,1465):
-  df.loc[ (df.Wteam == i), 'WteamAFOM']  = teams[ (teams.Team_Id == i)]['AFOM'].mean()
-  df.loc[ (df.Lteam == i), 'LteamAFOM']  = teams[ (teams.Team_Id == i)]['AFOM'].mean()
-  df.loc[ (df.Wteam == i), 'WteamClass'] = teams[ (teams.Team_Id == i)]['Team_Class'].mean()
-  df.loc[ (df.Lteam == i), 'LteamClass'] = teams[ (teams.Team_Id == i)]['Team_Class'].mean()
-df['PFOM'] = df.WteamAFOM/(df.WteamAFOM + df.LteamAFOM)
+  tourney.loc[ (tourney.Team1 == i), 'Team1AFOM1']  = teams[ (teams.Team_Id == i)]['AFOM1'].mean()
+  tourney.loc[ (tourney.Team2 == i), 'Team2AFOM1']  = teams[ (teams.Team_Id == i)]['AFOM1'].mean()
 
-#Once you've "predicted" the winner, need to be about 35% more confident in your prediction
-df['blah'] = (df.FOMW - df.WteamAFOM)/df.WteamAFOM
-corr = df[ (df.WteamClass == df.LteamClass) & (df.Season >= year - 10) & (df.Season < year) & (df.WteamAFOM > .3) ][['blah']].dropna().mean()
-df.loc[ (df.PFOM > 0.55), 'PFOM'] *= 1.+float(corr)
-df.loc[ (df.PFOM < 0.45), 'PFOM'] = 1.-(1.-df.PFOM)*(1.+float(corr))
+##---------------------  NOW CALCULATE PRED FOM FOR EACH REGULAR SEASON GAME ------------------------
+#Make base prediction
+tourney.loc[(abs(tourney.ClassDiff) < 4), 'PFOM1'] = tourney.Team1AFOM1/(tourney.Team1AFOM1+tourney.Team2AFOM1)
+#Now be more confident in prediction
+tourney.loc[ (tourney.PFOM1 > 0.5), 'Error'] = (tourney.FOM1 - tourney.PFOM1)/tourney.PFOM1
+tourney.loc[ (tourney.PFOM1 < 0.5), 'Error'] = -(tourney.FOM1 - tourney.PFOM1)/tourney.PFOM1
+corr = tourney[ (tourney.ClassDiff == 0) & (tourney.Season == year) & (tourney.Team1AFOM1 > .2) & (tourney.Team2AFOM1 < .8) ][['Error']].mean()
+tourney.loc[ (tourney.PFOM1 > 0.54), 'PFOM1'] *= 1.+float(corr)
+tourney.loc[ (tourney.PFOM1 < 0.46), 'PFOM1'] = 1.-(1.-tourney.PFOM1)*(1.+float(corr))
+#Finally, account for differences in class when early in the tournament
+tourney.loc[ (tourney.Daynum < 140) & (tourney.ClassDiff < 0), 'PFOM1'] = tourney.PFOM1**(1/(2*abs(tourney.ClassDiff)))
+tourney.loc[ (tourney.Daynum < 140) & (tourney.ClassDiff > 0), 'PFOM1'] = 1.-((1.-tourney.PFOM1)**(1/(2*abs(tourney.ClassDiff))))
 
-#Finally, need to correct prediction in case schools have different classes
-df['ClassDiff'] = (df.WteamClass - df.LteamClass)
-df.loc[ (df.ClassDiff < 0), 'PFOM' ] = df.PFOM**(1/(2.0*abs(df.ClassDiff)))
-df.loc[ (df.ClassDiff > 0), 'PFOM' ] = 1.-((1.-df.PFOM)**(1/(2.0*abs(df.ClassDiff))))
-print df[df.ClassDiff > 0][['Wscore', 'Lscore', 'ClassDiff', 'WteamAFOM', 'LteamAFOM', 'FOMW', 'PFOM']]
+#Now we're going to make this prediction using machine learning
+train = df[(df.Season == 2015) | (df.Season == 2014)][['FOM1','Team1AFOM1','Team2AFOM1','ClassDiff']]
+train_data = train.values
+tourney['ClassDiff'] = tourney.ClassDiff * (tourney.Daynum < 140)
 
-#---------------------  NOW CALCULATE BASED ON SEED -- signed (SEED1 - Seed2)^.3 - 0.7  ------------------------
+forest = RandomForestClassifier(n_estimators = 100)
+forest = forest.fit(train_data[0::,1::],train_data[0::,0].astype(str))
+test = tourney[tourney.Season == 2015][['FOM1','Team1AFOM1','Team2AFOM1','ClassDiff']]
+test_data = test.values
+output = forest.predict(test_data[0::,1::]).astype(float)
+tourney.loc[ (tourney.Season == 2015), 'CPFOM'] = output
+tourney.loc[ (tourney.Season == 2015), 'Myerr'] = (tourney.PFOM1 - tourney.FOM1)/tourney.FOM1
+tourney.loc[ (tourney.Season == 2015), 'Coerr'] = (tourney.CPFOM - tourney.FOM1)/tourney.FOM1
+tourney.loc[ (tourney.Season == 2015) & ((tourney.CPFOM > .3) | (tourney.CPFOM < .7)), 'PFOM'] = tourney.PFOM1
+tourney.loc[ (tourney.Season == 2015) & ((tourney.CPFOM < .3) | (tourney.CPFOM > .7)), 'PFOM'] = tourney.CPFOM
 
-#---------------------  NOW CALCULATE AFOM of previos team 1 - team 2 matchups          ------------------------
+#---------------------  NOW CALCULATE BASED ON SEED -- signed (SEED1 - Seed2)           ------------------------
+for i in range(1101,1465):
+  tourney.loc[ (tourney.Team1 == i), 'Team1Seed']  = seeds[ (seeds.Season == year) & (seeds.Team == i)]['Seed'].mean()
+  tourney.loc[ (tourney.Team2 == i), 'Team2Seed']  = seeds[ (seeds.Season == year) & (seeds.Team == i)]['Seed'].mean()
+tourney['SeedDiff'] = tourney['Team1Seed'] - tourney['Team2Seed']
+print tourney[tourney.Season == year][['Team1','Team2','ClassDiff','Team1score','Team2score','FOM1','PFOM', 'SeedDiff','Team1isHome']].head(30)
 
 #---------------------  NOW CALCULATE HOMEFIELD ADVANTAGE  ----------------------------------------------------
 
 
 #--------------------  NOW CALCULATE WINNINGNESS PREDICTIONS ----------------------------------------------------
 
-
-
-
-
-
-
-
-
-
-
-
-##Origin - cleaned Embarked
-#df['Origin'] = df.Embarked.dropna().map( { 'Q': 1, 'S': 2, 'C': 3 } ).astype(int)
-#
-##Calculate the median age in each class/gender
-#median_ages = np.zeros((2,3))
-#for i in range(0, 2):
-#  for j in range(0, 3):
-#    median_ages[i,j] = df[ (df.Gender == i) & (df.Pclass == j+1) ]['Age'].dropna().median()
-#
-##Make new column for filled age
-#df['AgeFill'] = df.Age
-##print df[ df.Age.isnull() ][['Gender', 'Pclass', 'Age', 'AgeFill']].head(10)
-#for i in range(0, 2):
-#  for j in range(0, 3):
-#    df.loc[ (df.Age.isnull()) & (df.Gender == i) & (df.Pclass == j+1), 'AgeFill'] = median_ages[i,j]
-##print df[ df.Age.isnull() ][['Gender', 'Pclass', 'Age', 'AgeFill']].head(10)
-#df['AgeIsNull'] = pd.isnull(df.Age).astype(int)
-#
-##Family size variable
-#df['FamilySize'] = df['SibSp'] + df['Parch']
-#
-##Age*Class variable
-#df['Age*Class'] = df.AgeFill * df.Pclass
-#
-##Drop the columns we will not use
-#train_ids = df['PassengerId'].values
-#df = df.drop( ['Name', 'Sex', 'Ticket', 'Cabin', 'Embarked', 'Age', 'Fare', 'PassengerId'], axis=1) 
-#
-##Dump the info so we can see if any are non-null
-##print df.info()
-#
-##See which origin is most common
-##print "mode: ", df.Origin.mean(), df.Origin.mode()
-##for i in range(1,4):
-##  print "origin in ", i, " for ", df[ df.Origin == i ]['Origin'].count(), " passengers"
-#
-##Most common origin is 2, so set that for any if origin is not filled
-#df.loc[ df.Origin.isnull(), 'Origin'] = 2 
-#
-##Define train data
-#train_data = df.values
-#
-##And same thing for the test values
-#test_df = pd.read_csv('test.csv', header=0)        # Load the test file into a dataframe
-#test_df['Gender'] = test_df.Sex.map( { 'female': 0, 'male': 1} ).astype(int)
-#test_df['Origin'] = test_df.Embarked.dropna().map( { 'Q': 1, 'S': 2, 'C': 3 } ).astype(int)
-#test_df['AgeFill'] = test_df.Age
-#for i in range(0, 2):
-#  for j in range(0, 3):
-#    test_df.loc[ (test_df.Age.isnull()) & (test_df.Gender == i) & (test_df.Pclass == j+1), 'AgeFill'] = median_ages[i,j]
-#test_df['AgeIsNull'] = pd.isnull(test_df.Age).astype(int)
-#test_df['FamilySize'] = test_df['SibSp'] + test_df['Parch']
-#test_df['Age*Class']  = test_df.AgeFill  * test_df.Pclass
-#ids = test_df['PassengerId'].values
-#test_df = test_df.drop( ['Name', 'Sex', 'Ticket', 'Cabin', 'Embarked', 'Age', 'Fare', 'PassengerId'], axis=1) 
-#test_df.loc[ test_df.Origin.isnull(), 'Origin'] = 2 
-#test_data = test_df.values
-#
-##Run it
-#forest = RandomForestClassifier(n_estimators = 100)
-#forest = forest.fit(train_data[0::,1::],train_data[0::,0])
-##output = forest.predict(test_data).astype(int)
-#output = forest.predict(train_data[0::,1::]).astype(int)
-#
-##Write out values
-#predictions_file = open("myfirstforest.csv", "wb")
-#open_file_object = csv.writer(predictions_file)
-#open_file_object.writerow(["PassengerId","Survived"])
-#open_file_object.writerows(zip(train_ids, output, df.Survived, output == df.Survived))
-#predictions_file.close()
-#
-##Final Score
-#good = 0
-#total = 0
-#for i in range(df.Survived.count()):
-#  if (output[i] == df.Survived[i]): good += 1
-#  total += 1
-#print float(good)/float(total)
